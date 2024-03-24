bb49904bd913f59ae98f52ae0bfe8ad8c5727e1f
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-03-24 08:36:47 +0000

    ...

---
+++ Test 1.py
@@ -0,0 +1,18 @@
+# define the terms!
+
+# 1) Numbers -> Numbers in python is a numeric data types which are int, float etc.
+
+# 2) Strings -> Strings in python i the collection of alphabats, words or other characters.
+
+# 3) Lists -> List is a data structure which is a collections of items in which we can store like -:
+#             String, Float, Intger.  In Lists elements are always written in square brackets "[]"
+#             and each items are sapared by comma ",".
+
+# 4) Tuples-> Tuples is a data structure which is also a collection of items, in which we can 
+#             store multiple values like -: integer, float, string. In Tuples elements are
+#             written in parenthesis "()" and each items are saparated by comma ",".
+
+# 5) Dictionary -> Dictionary is a data structure in which elements are written in key - value 
+#                  pair. example -: {"Name" : "Surbhi"}. In Dictionary elements are written in
+#                  curly braces "{}" and each pair of items are highlighted with double inverted
+#                  comma " ".
