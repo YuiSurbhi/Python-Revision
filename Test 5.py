ef2fb3ef8617ead8a0c2f98ce3cb73244b8fd98e
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-04-03 10:51:46 +0000

    ...

--- Test 5.py
+++ Test 5.py
@@ -12,3 +12,12 @@
 #"grab the 'hello'"
 #print(d['k1']['k2']);
 
+# 3.
+#d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+#"grab the 'hello'"
+#print(d['k1'][0]['nest_key'][1][0]);
+
+# 4.
+d = {'k1':[1,2,{'k2':{'this is tricky',{'tough':[1,2,['hello']]}}}]}
+#"grab the 'hello'"
+print(['k1'][2]['k2'][1]['tough'][2][0]);
