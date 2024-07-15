634cf503f4036a4f1a348bc646cac3e698eb2da0
Surbhi Verma <163987279+YuiSurbhi@users.noreply.github.com>
2024-07-15 15:37:09 +0000

    commit

--- Test 5.py
+++ Test 5.py
@@ -2,22 +2,22 @@
 
 # 1) Using keys and indexing, grab the 'hello' from the following dictionaries:
 
-# 1.
-#d = {'simple_key' : 'hello'}   
-#"grab the 'hello'"
-#print(d['simple_key']);
+# Dict 1.
+d = {'simple_key' : 'hello'}    
+# grabbing the (hello) 
+print(d['simple_key']);
 
-# 2.
-#d = {'k1':{'k2':'hello'}}
-#"grab the 'hello'"
-#print(d['k1']['k2']);
+# Dict 2.
+d = {'k1':{'k2':'hello'}}  
+# grabbing the (hello)  
+print(d['k1']['k2']);
 
-# 3.
-#d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
-#"grab the 'hello'"
-#print(d['k1'][0]['nest_key'][1][0]);
+# Dict 3.
+d = {'k1':[{'nest_key':['this is deep',['hello']]}]}     
+# grabbing the (hello)
+print(d['k1'][0]['nest_key'][1][0]);
 
-# 4.
+# Dict 4.
 d = {'k1':[1,2,{'k2':{'this is tricky',{'tough':[1,2,['hello']]}}}]}
-#"grab the 'hello'"
+# grabbing the (hello) 
 print(['k1'][2]['k2'][1]['tough'][2][0]);
